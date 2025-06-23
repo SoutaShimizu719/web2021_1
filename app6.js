9ae0630cf50c0c3f1557597cb2d3f6d65e920f71
SoutaShimizu719 <s2132066rz@s.chibakoudai.jp>
2025-06-23 11:47:07 +0900

    各都道府県の詳細がわかるページに飛べるリンクを作成した　また、データを追加するプログラムを作成した

--- app6.js
+++ app6.js
@@ -7,6 +7,8 @@ const db2 = new sqlite3.Database('test2.db');
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const message = "Hello world";
@@ -20,6 +22,21 @@ app.get("/db", (req, res) => {
                 res.render('show', {mes:"エラーです"});
             }
             res.render('select', {data:row});
+        })
+    })
+})
+
+app.get("/db/:id", (req, res) => {
+    console.log( req.params );
+    db.serialize( () => {
+        const sql = "select id, 都道府県, 人口, 大学 from example where id=" + req.params.id + ";";
+        console.log(sql);
+        db.all(sql , (error, row) => {
+            if( error ) {
+                res.render('show', {mes:"エラーです"});
+            }
+            console.log(row);
+            res.render('db', {data:row});
         })
     })
 })
@@ -95,6 +112,24 @@ app.get("/game", (req, res) => {
         })
     })
 })
+
+app.post("/insert", (req, res) => {
+    let sql = `
+    insert into example (都道府県,人口,大学) values ("` + req.body.name + `",` + req.body.jinko + `,` + req.body.daigaku + `);
+    `
+    console.log(sql);
+    db.serialize( () => {
+        db.run( sql, (error, row) => {
+            console.log(error);
+            if(error) {
+                res.render('show', {mes:"エラーです"});
+            }
+            res.redirect('/db');
+        });
+    });
+    console.log(req.body);
+});
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
